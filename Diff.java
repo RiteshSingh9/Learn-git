7720620a69be85e12e5a960bca0097b6c4218ae7
RiteshSingh9 <raunak.singh.visen.9@gmail.com>
2021-10-02 16:28:19 +0530

    Mudified Diff.java

--- Diff.java
+++ Diff.java
@@ -1,5 +1,5 @@
 public class Diff {
     public static void main(String[] args) {
-        System.out.println("This is Diff.java");
+        System.out.println("This is Diff.java modified");
     }
 }
